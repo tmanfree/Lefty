1308f1e1e97f96931c06c8833a4bb41fc50deafd
Thomas <40835884+tmanfree@users.noreply.github.com>
2018-11-22 08:49:15 -0700

    Modified to act as a submodule (or by itself)

--- Lefty.py
+++ Lefty.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-from netmiko import ConnectHandler
-import argparse
-import re
-import config
 
+from netmiko import ConnectHandler
+import re
 import sys
 
-#Recommended usage Lefty 10.0.0.1 1ef6
 
-def normalize_mac( address):
+# Howdy Neighbour!
+
+# Recommended usage Lefty 10.0.0.1 1ef6
+
+def normalize_mac(address):
     tmp3 = address.lower().translate({ord(":"): "", ord("-"): "", ord(" "): ""})
 
     if len(tmp3) % 4 == 0:
         return '.'.join(a + b + c + d for a, b, c, d in
-                    zip(tmp3[::4], tmp3[1::4], tmp3[2::4], tmp3[3::4]))  # insert a period every four chars
+                        zip(tmp3[::4], tmp3[1::4], tmp3[2::4], tmp3[3::4]))  # insert a period every four chars
     else:
         if len(tmp3) < 4:
             return tmp3
@@ -21,112 +22,111 @@ def normalize_mac( address):
             print("Please enter a mac address in a group of 4")
             sys.exit()
 
-###################### Begin main program###############################
 
-config.load_sw_base_conf()
+def mac_search(ipaddr, macaddr, username, password):
+    input_vals = {'IP': ipaddr, 'mac': normalize_mac(macaddr)}
 
+    # Regexs
+    reg_PC = re.compile(r'( Po\d)')  # Group 0: search for Port Channel
+    reg_PC_port = re.compile(r'..(\d/)*\d/\d{1,2}')  # Group 0: Port in port channel (or mac address table)
+    reg_mac_addr = re.compile(r'....\.....\.....')  # Group 0: Port in port channel (or mac address table)
+    reg_CDP_Phone = re.compile(r'Phone')  # Group 0: Check CDP neigh for Phone
+    reg_IP_addr = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')  # Group 0: Check for IP
 
-    ####adding CLI Parsing
-parser = argparse.ArgumentParser(description='Navigate mac address tables to find a specified MAC.')
-parser.add_argument('startip', metavar='IP',
-                    help='The IP to start looking for the mac address at')
-parser.add_argument('macaddr', metavar='MAC',
-                    help='The MAC address to search for ')
+    print("IP Address:{}".format(input_vals['IP']))
+    print("MAC Address:{}".format(input_vals['mac']))
 
-args = parser.parse_args()
-input_vals = {'IP':args.startip,'mac': normalize_mac(args.macaddr)}
-#### Done CLI Parsing
+    # IP Verification - Not functional at the moment
+    # ping = str(subprocess.run(['ping','-c','1',in_ip_add], stdout=subprocess.DEVNULL))
+    # while ('returncode=1' in ping):
+    #    print('Host is not Reachable')
+    #    in_ip_add = input('Enter the IP/FQDN: ')
+    #    ping = str(subprocess.run(['ping','-c','1',in_ip_add], stdout=subprocess.DEVNULL))
 
-in_ip_add = input_vals['IP']
-in_mac_add = input_vals['mac']
+    next_ip = input_vals['IP']
 
-# Regexs
-reg_PC = re.compile(r'( Po\d)') #Group 0: search for Port Channel
-reg_PC_port = re.compile(r'..(\d/)*\d/\d{1,2}') #Group 0: Port in port channel (or mac address table)
-reg_mac_addr = re.compile(r'....\.....\.....') #Group 0: Port in port channel (or mac address table)
-reg_CDP_Phone = re.compile(r'Phone') #Group 0: Check CDP neigh for Phone
-reg_IP_addr = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}') #Group 0: Check for IP
+    ### START OF LOOOP - loop to the bottom most port that the MAC is found on
+    while next_ip is not 0:
+        print('------- CONNECTING to switch {}-------'.format(next_ip))
 
-print ("IP Address:{}".format(in_ip_add))
-print ("MAC Address:{}".format(in_mac_add))
+        # Switch Parameters
+        cisco_sw = {
+            'device_type': 'cisco_ios',
+            'ip': next_ip,
+            'username': username,
+            'password': password,
+            'port': 22,
+            'verbose': False,
+        }
+        # zero out next ip. This will be compared to see if there is another switch required to jump into
+        current_ip = next_ip
+        next_ip = 0
 
+        # SSH Connection
+        net_connect = ConnectHandler(**cisco_sw)
+        if net_connect:
+            ### ADD ERROR HANDLING FOR FAILED CONNECTION
+            print("-------- CONNECTED --------")
 
-#IP Verification - Not functional at the moment
-#ping = str(subprocess.run(['ping','-c','1',in_ip_add], stdout=subprocess.DEVNULL))
-#while ('returncode=1' in ping):
-#    print('Host is not Reachable')
-#    in_ip_add = input('Enter the IP/FQDN: ')
-#    ping = str(subprocess.run(['ping','-c','1',in_ip_add], stdout=subprocess.DEVNULL))
+            # Show Interface Status
+            output = net_connect.send_command('show mac address-table | i ' + input_vals['mac'])
+            macHolder = reg_mac_addr.search(output)
 
-next_ip = in_ip_add
+            if macHolder is not None:
 
-### START OF LOOOP - loop to the bottom most port that the MAC is found on
-while next_ip is not 0 :
-    print ('------- CONNECTING to switch {}-------'.format(next_ip))
-
-    # Switch Parameters
-    cisco_sw = {
-        'device_type': 'cisco_ios',
-        'ip':   next_ip,
-        'username': config.username,
-        'password': config.password,
-        'port' : 22,
-        'verbose': False,
-    }
-    #zero out next ip. This will be compared to see if there is another switch required to jump into
-    current_ip = next_ip
-    next_ip=0
-
-    # SSH Connection
-    net_connect = ConnectHandler(**cisco_sw)
-    if net_connect:
-        ### ADD ERROR HANDLING FOR FAILED CONNECTION
-        print ("-------- CONNECTED --------")
-
-        # Show Interface Status
-        output = net_connect.send_command('show mac address-table | i '+ in_mac_add)
-        macHolder = reg_mac_addr.search(output)
-
-        if macHolder is not None:
-
-            #if a port channel then-->
-            if "Po" in output:
-                reg_find = reg_PC.search(output)
-                output = net_connect.send_command('show etherchannel summary | include '+ reg_find.group(0))
-            port_reg_find = reg_PC_port.search(output)
-            #currently the following command gets multiple matches on things like 1/0/1 (11,12,13, etc)
-            port_info = net_connect.send_command("show int status | i {} ".format(port_reg_find.group(0)))
-            output = net_connect.send_command("show cdp neigh {} detail".format(port_reg_find.group(0)))
-            #check if the cdp neigh information is a phone
-            reg_find = reg_CDP_Phone.search(output)
-            #if it is a phone....
-            if reg_find is not None:
-                print("the furthest downstream location is: {} on switch IP:{}".format(port_reg_find.group(0), current_ip))
-                print("port info: {}".format(port_info))
-            else:
-                #look for an IP address in CDP neighbour
-                reg_find = reg_IP_addr.search(output)
-                #if an IP is found, it is a switch (assuming it isn't an AP)
+                # if a port channel then-->
+                if "Po" in output:
+                    reg_find = reg_PC.search(output)
+                    output = net_connect.send_command('show etherchannel summary | include ' + reg_find.group(0))
+                port_reg_find = reg_PC_port.search(output)
+                # currently the following command gets multiple matches on things like 1/0/1 (11,12,13, etc)
+                port_info = net_connect.send_command("show int status | i {} ".format(port_reg_find.group(0)))
+                output = net_connect.send_command("show cdp neigh {} detail".format(port_reg_find.group(0)))
+                # check if the cdp neigh information is a phone
+                reg_find = reg_CDP_Phone.search(output)
+                # if it is a phone....
                 if reg_find is not None:
-                    print ("Mac {} found on port {}".format(macHolder.group(0),port_reg_find.group(0)))
-                    next_ip = reg_find.group(0) # assign the next IP and continue
-                #no CDP info is there, it should be a client port
-                else:
-                    #port_info = net_connect.send_command("show int status | i {} ".format(port_reg_find.group(0)))
-                    print("the furthest downstream location is: {} on switch IP:{}".format(port_reg_find.group(0), current_ip))
+                    print("the furthest downstream location is: {} on switch IP:{}".format(port_reg_find.group(0),
+                                                                                           current_ip))
                     print("port info: {}".format(port_info))
+                else:
+                    # look for an IP address in CDP neighbour
+                    reg_find = reg_IP_addr.search(output)
+                    # if an IP is found, it is a switch (assuming it isn't an AP)
+                    if reg_find is not None:
+                        print("Mac {} found on port {}".format(macHolder.group(0), port_reg_find.group(0)))
+                        next_ip = reg_find.group(0)  # assign the next IP and continue
+                    # no CDP info is there, it should be a client port
+                    else:
+                        # port_info = net_connect.send_command("show int status | i {} ".format(port_reg_find.group(0)))
+                        print("the furthest downstream location is: {} on switch IP:{}".format(port_reg_find.group(0),
+                                                                                               current_ip))
+                        print("port info: {}".format(port_info))
+
+                #    print ("output = {}".format(output))
+                #    print ("find = {}".format(reg_find))
+                #    print ("formatted = {}".format(reg_find.group(0)))
+
+                # Close Connection
+                net_connect.disconnect()
+            else:
+                print("Mac address not found.")
+    print("-------- COMPLETE --------")
+    # print ('############################')
 
 
+# if being run by itself
+if __name__ == "__main__":
+    # import files to load config and parse CLI
+    import config
+    import argparse
 
-        #    print ("output = {}".format(output))
-        #    print ("find = {}".format(reg_find))
-        #    print ("formatted = {}".format(reg_find.group(0)))
-
-            # Close Connection
-            net_connect.disconnect()
-        else:
-            print("Mac address not found.")
-print ("-------- COMPLETE --------")
-#print ('############################')
-
+    config.load_sw_base_conf()
+    parser = argparse.ArgumentParser(description='Navigate mac address tables to find a specified MAC.')
+    parser.add_argument('startip', metavar='IP',
+                        help='The IP to start looking for the mac address at')
+    parser.add_argument('macaddr', metavar='MAC',
+                        help='The MAC address to search for ')
+    args = parser.parse_args()
+    mac_search(args.startip, normalize_mac(args.macaddr), config.username, config.password)
 #
